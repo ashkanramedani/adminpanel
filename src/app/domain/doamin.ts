afc272703bb523217315d34b2b3c157bf9f7f7bd
ashkan_ramedani <ashkanramedani1370@gmail.com>
2024-03-07 16:50:12 +0330

    update library

--- src/app/domain/doamin.ts
+++ src/app/domain/doamin.ts
@@ -2,15 +2,26 @@ import { environment } from '../../environments/environment';
 export const Domain =
 {
     "WebsiteUrl": "",
+    "GetTagList": `${environment.apiUrl}tag/all`,
+
     "GetPost": `${environment.apiUrl}post`,
     "GetPostTopic": `${environment.apiUrl}post/get_post_topic`,
     "GetPostCategory": `${environment.apiUrl}category/all`,
-    "GetTagList": `${environment.apiUrl}tag/all`,
     "DeletePost": `${environment.apiUrl}post`,
     "CreatePost": `${environment.apiUrl}post`,
     "PatchPost": `${environment.apiUrl}post/update`,
     "ChangeGroupStatus": `${environment.apiUrl}post/group-status-modifications-in-group-post`,
     "GetSinglePost": `${environment.apiUrl}post/{topic}`,
+
+    "GetLibrary": `${environment.apiUrl}library`,
+    "GetLibraryTopic": `${environment.apiUrl}library/get_library_topic`,
+    "GetLibraryCategory": `${environment.apiUrl}category/all`,   
+    "DeleteLibrary": `${environment.apiUrl}library`,
+    "CreateLibrary": `${environment.apiUrl}library`,
+    "PatchLibrary": `${environment.apiUrl}library/update`,
+    "ChangeGroupLibraryStatus": `${environment.apiUrl}library/group-status-modifications-in-group-library`,
+    "GetSingleLibrary": `${environment.apiUrl}library/{topic}`,
+
     "GetEmployees": `${environment.apiUrl}user/employees`,
     "GroupDeletePost": `${environment.apiUrl}post`,
     "GetTeachersDelay": `${environment.apiUrl}form/tardy_request/search`,
--- src/app/interfaces/ILibrary.ts
+++ src/app/interfaces/ILibrary.ts
@@ -1,4 +1,19 @@
 export interface ILibrary
 {
-    
+    library_name: string;
+    library_image: string;
+    library_type: string;
+    library_description: string;
+    library_summer: string;
+    library_audio_file_link: string;
+    library_audio_file_path: string;
+    library_aparat_video_id: string;
+    library_aparat_video_code: string;
+    library_video_file_link: string;
+    library_video_file_path: string;
+    library_data_file_link: string;
+    library_data_file_path: string;
+    priority: number;
+    library_pk_id: number;
+    download_count: number;
 }
